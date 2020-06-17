c7c202decff764f8675d0448534a77a8918abdac
claire froelich <claire.froelich@gmail.com>
2020-06-17 12:35:57 -0400

    Updates

---
+++ precache-manifest.993b3403c3ac0b68ffc61e62c1ac5b47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2e8e62c44d532bfc4fa9a5d0de3262",
+    "url": "/how-much-dough/index.html"
+  },
+  {
+    "revision": "e4c795e3f125a735987e",
+    "url": "/how-much-dough/static/css/main.75df9f48.chunk.css"
+  },
+  {
+    "revision": "6850bee036e1f1995c9d",
+    "url": "/how-much-dough/static/js/2.c70623b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/how-much-dough/static/js/2.c70623b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c795e3f125a735987e",
+    "url": "/how-much-dough/static/js/main.94ffbe5a.chunk.js"
+  },
+  {
+    "revision": "4634c69aea3fca56b771",
+    "url": "/how-much-dough/static/js/runtime-main.de3f6347.js"
+  },
+  {
+    "revision": "cf18b6297c3065e52d11afe04a8b6110",
+    "url": "/how-much-dough/static/media/mintbean.cf18b629.png"
+  }
+]);
