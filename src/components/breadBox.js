46f094cd825ae3854d56ae623b06974b537ef7c3
claire froelich <claire.froelich@gmail.com>
2020-06-17 16:03:02 -0400

    making dough

---
+++ src/components/breadBox.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+
+import {convert} from '../utils/convert'
+
+const BreadBox = ({ referenceField, rates }) => {
+  const [breads, setBreads] = useState(0)
+  const [usd, setUsd] = useState(0)
+
+  useEffect(()=> {
+    if (referenceField.code !== 'USD') {
+      const usdAmount = convert(referenceField.amount, referenceField.code, 'USD', rates)
+      setUsd(usdAmount)
+    } else {
+      setUsd(referenceField.amount)
+    }
+
+  },[referenceField])
+
+  useEffect(()=> {
+    // 1 bread = 2.55 USD
+    setBreads(usd/2.55)
+  },[usd])
+
+  return (
+    <div className="bread-box">
+    {breads > 0 &&
+      <h3>That's {breads} loafs of bread in the USA</h3>
+    }
+    <div className="breads">
+    {'🍞'.repeat(breads)}
+
+    </div>
+    </div>
+  );
+}
+
+export default BreadBox;
--- src/components/currencyConverter.js
+++ src/components/currencyConverter.js
@@ -3,6 +3,7 @@ import SVG from 'react-inlinesvg'
 
 import { convert } from '../utils/convert'
 
+import BreadBox from './breadBox'
 import breadOutline from '../static/bread-outline.svg'
 
 const endpoint = 'https://openexchangerates.org/api/latest.json?app_id='
@@ -15,8 +16,8 @@ const CurrencyConverter = ({ baseCur, targetCur }) => {
   const [field1, setField1] = useState({ amount: 0, code: 'USD' })
   const [field2, setField2] = useState({ amount: 0, code: 'JPY' })
   const [baseField, setBaseField] = useState(1)
-  const [baseRate, setBaseRate] = useState(1)
-  const [adjTargetRate, setAdjTargetRate] = useState(1)
+  // const [baseRate, setBaseRate] = useState(1)
+  // const [adjTargetRate, setAdjTargetRate] = useState(1)
 
   const field1Input = useRef()
   const field2Input = useRef()
@@ -108,49 +109,54 @@ const CurrencyConverter = ({ baseCur, targetCur }) => {
 
   return (
     <div className="currency-converter">
-      <SVG className="bread-outline" src={breadOutline} />
-      <div className="input-section">
-        <input
-          ref={field1Input}
-          type="number"
-          onChange={()=>onChange(1)}
-          onFocus={()=> baseField === 1 ? null : setBaseField(1)}
-          className="input-amount"
-          value={field1.amount}
-          id="input-1"/>
-        <select
-          ref={field1Code}
-          name="code1"
-          onChange={()=>onChange(1)}
-          onFocus={()=> baseField === 1 ? null : setBaseField(1)}
-          value={field1.code}
-          id="code1"
-        >
-          {codes.map(c => (
-            <option key={c} value={c}>{c}</option>
-          ))}
-        </select>
+      <div className="top">
+        <SVG className="bread-outline" src={breadOutline} />
+        <div className="input-section">
+          <input
+            ref={field1Input}
+            type="number"
+            onChange={()=>onChange(1)}
+            onFocus={()=> baseField === 1 ? null : setBaseField(1)}
+            className="input-amount"
+            value={field1.amount}
+            id="input-1"/>
+          <select
+            ref={field1Code}
+            name="code1"
+            onChange={()=>onChange(1)}
+            onFocus={()=> baseField === 1 ? null : setBaseField(1)}
+            value={field1.code}
+            id="code1"
+          >
+            {codes.map(c => (
+              <option key={c} value={c}>{c}</option>
+            ))}
+          </select>
+        </div>
+        <div className="input-section">
+          <input
+            ref={field2Input}
+            type="number"
+            onFocus={()=> baseField === 2 ? null : setBaseField(2)}
+            onChange={()=>onChange(2)}
+            className="input-amount"
+            value={field2.amount}
+            id="input-2"/>
+          <select
+            ref={field2Code}
+            name="code2"
+            onFocus={()=> baseField === 2 ? null : setBaseField(2)}
+            onChange={()=>onChange(2)}
+            value={field2.code}
+            id="code2">
+            {codes.map(c => (
+              <option key={c} value={c}>{c}</option>
+            ))}
+          </select>
+        </div>
       </div>
-      <div className="input-section">
-        <input
-          ref={field2Input}
-          type="number"
-          onFocus={()=> baseField === 2 ? null : setBaseField(2)}
-          onChange={()=>onChange(2)}
-          className="input-amount"
-          value={field2.amount}
-          id="input-2"/>
-        <select
-          ref={field2Code}
-          name="code2"
-          onFocus={()=> baseField === 2 ? null : setBaseField(2)}
-          onChange={()=>onChange(2)}
-          value={field2.code}
-          id="code2">
-          {codes.map(c => (
-            <option key={c} value={c}>{c}</option>
-          ))}
-        </select>
+      <div className="bottom">
+        <BreadBox referenceField={field1} rates={rates.rates} />
       </div>
     </div>
   )
